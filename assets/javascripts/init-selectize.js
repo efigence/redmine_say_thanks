0b8338942a09cd9db41d42a50a41d095ecee0007
Jacek Grzybowski <jgrzybowski@artegence.com>
2015-05-07 17:03:06 +0200

    cleanup

---
+++ assets/javascripts/init-selectize.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.selectize').selectize({
+    sortField: 'text'
+  });
+  $('.selectize-with-btn').selectize({
+    plugins: ['remove_button']
+  });
+});
---
+++ assets/javascripts/settings.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('.selectize').selectize({
+    plugins: ['remove_button']
+  });
+  $('.settings').removeClass('tabular');
+})
