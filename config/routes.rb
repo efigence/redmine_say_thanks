a969f4a4161a158a5f80f41462684c0cda5dbf48
Jacek Grzybowski <jgrzybowski@artegence.com>
2015-05-07 16:26:41 +0200

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,13 @@ resources :thanks, only: [:new, :create] do
     get 'received' => "thanks#received"
     resources :rewards, only: :index, as: 'thanks_rewards'
   end
-
-
-
   member do
     put 'unroll' => "thanks#unroll"
   end
 end
 
-
 scope :thanks do
   scope :manager do
-
     scope ':group_id' do
       get '/' => "manager_thanks#index", as: 'manager_thanks'
       get 'stats' => "manager_thanks#stats", as: 'manager_thanks_stats'
